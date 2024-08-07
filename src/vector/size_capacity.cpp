914c3b71f2e397643d360cdc5e893b19b541d478
naar-kie <kgadile.eze@gmail.com>
2024-08-08 01:22:14 +0200

    Exercise 2.1

--- src/vector/size_capacity.cpp
+++ src/vector/size_capacity.cpp
@@ -20,3 +20,7 @@ int main()
 
     return 0;
 }
+
+//--------------------------Exercise 2.1-------------------------------
+// 1. a vector size refers to the number of elements stored in a vector. vector's capacity refers to the number of elements that a vector can carry at the current memory allocation
+// 2. By doubling, a vector's capacity grows to accommodate more elements efficiently, minimizing the need for frequent memory allocations and copies as new elements are added.
